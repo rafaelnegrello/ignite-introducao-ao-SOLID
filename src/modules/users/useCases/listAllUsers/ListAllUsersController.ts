9205589c0f8558d2d04704d1249aae65ec0c4cfd
Rafael Negrello <rafael.negrello@gmail.com>
2021-04-18 00:39:36 -0300

    feat: implementing challenge

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,15 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    let { user_id } = request.headers;
+    try {
+      user_id = user_id.toString();
+      const all = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.json(all);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
